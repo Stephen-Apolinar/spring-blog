b00a548c20aa5b77ce762b60feee64891cf353e9
Stephen Apolinar <stephenapolinar@Stephens-MacBook-Pro.local>
2018-09-06 16:47:06 -0500

    views

---
+++ src/main/java/com/blog/blog/DiceController.java
@@ -0,0 +1,27 @@
+package com.blog.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice")
+    public String diceRoll() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String guessOne(@PathVariable int guess, Model viewModel) {
+        Random random = new Random();
+        int diceNumber = random.nextInt((6 - 1) + 1) + 1;
+        viewModel.addAttribute("guess", guess);
+        viewModel.addAttribute("diceNumber", diceNumber);
+
+        return "roll-results";
+    }
+
+}
--- src/main/java/com/blog/blog/PostController.java
+++ src/main/java/com/blog/blog/PostController.java
@@ -1,22 +1,37 @@
 package com.blog.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
+    List<Post> posts = new ArrayList<>();
 
+    public PostController() {
+
+        posts.add(new Post("one", "post number one"));
+        posts.add(new Post("two", "this is the second post"));
+        posts.add(new Post("three", "here's the third post"));
+
+    }
     //  Bundled together individual post and many post viewing to the same url
     @GetMapping("/posts")
-    @ResponseBody
-    private String posts(@RequestParam(value = "id", defaultValue = "All") String id){
+    public String index(Model viewModel){
+        viewModel.addAttribute("posts", posts);
+        return "posts/index";
+    }
 
-        if(id.equals("All")){
-            return "This will be the index page.";
-        }
+    @GetMapping("/posts/{id}")
+    public String show ( @PathVariable long id, Model viewModel){
+        Post post = posts.get((int) id - 1);
+        viewModel.addAttribute("post", post);
+        return "posts/show";
 
-        return "This will be the individual post page for id number " + id + " .";
     }
 
     //    Has unnecessary functionality for testing query strings
