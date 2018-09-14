8c3ed72505cdb7586eda4540a8cdd62c3e03130c
Stephen Apolinar <stephenapolinar@Stephens-MacBook-Pro.local>
2018-09-14 14:33:18 -0500

    working

--- src/main/java/com/blog/blog/models/User.java
+++ src/main/java/com/blog/blog/models/User.java
@@ -7,8 +7,7 @@ import java.util.List;
     @Table(name="user")
     public class User {
 
-        @Id
-        @GeneratedValue
+        @Id @GeneratedValue
         private long id;
 
         @Column(nullable = false, unique = true)
@@ -18,47 +17,35 @@ import java.util.List;
         private String email;
 
         @Column(nullable = false)
-        private String firstName;
-
-        @Column(nullable = false)
-        private String lastName;
-
-        @Column(nullable = false)
         private String password;
 
-
         @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
-        private List<Post> post;
+        private List<Post> posts;
 
-
-        public User(long id, String username, String email, String password, String firstName, String lastName) {
-            this.id = id;
-            this.username = username;
-            this.email = email;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.password = password;
+        public User() {
         }
 
-        public User(String username, String email, String password, String firstName, String lastName) {
+        public User(String username, String email, String password, List<Post> posts) {
             this.username = username;
             this.email = email;
-            this.firstName = firstName;
-            this.lastName = lastName;
             this.password = password;
+            this.posts = posts;
+        }
+
+        public User(long id, String username, String email, String password, List<Post> posts) {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.id = id;
+            this.posts = posts;
         }
 
         public User(User copy) {
             id = copy.id; // This line is SUPER important! Many things won't work if it's absent
-            username = copy.username;
             email = copy.email;
-            firstName = copy.firstName;
-            lastName = copy.lastName;
+            username = copy.username;
             password = copy.password;
         }
-
-        public User() {}
-
 
         public long getId() {
             return id;
@@ -92,28 +79,11 @@ import java.util.List;
             this.password = password;
         }
 
-
-        public List<Post> getPost() {
-            return post;
+        public List<Post> getPosts() {
+            return posts;
         }
 
-        public void setPost(List<Post> post) {
-            this.post = post;
-        }
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
+        public void setPosts(List<Post> posts) {
+            this.posts = posts;
         }
 }
--- src/main/java/com/blog/blog/repositories/UserRepo.java
+++ src/main/java/com/blog/blog/repositories/UserRepo.java
@@ -1,9 +1,15 @@
 package com.blog.blog.repositories;
 
 import com.blog.blog.models.Post;
+import com.blog.blog.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends CrudRepository<Post, Long> {
+    User findById(long id);
+
+    User findByUsername(String name);
+
+    void save(User user);
 }
